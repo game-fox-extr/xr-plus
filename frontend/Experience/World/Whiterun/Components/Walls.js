c70272f7736243f6c7e029e3500d1ea900d03a47
Shuddown <shudheshkibhakt@gmail.com>
2024-11-02 22:47:12 +0530

    Reverted Back

--- frontend/Experience/World/Whiterun/Components/Walls.js
+++ frontend/Experience/World/Whiterun/Components/Walls.js
@@ -13,23 +13,22 @@ export default class Walls {
 
     init() {
         this.walls = this.resources.items.walls.scene;
-        // if (window.mobileAndTabletCheck()) {
-        //     this.walls_texture = this.resources.items.mobile_walls_texture; // mobile texture
-        // } else {
-        //     this.walls_texture = this.resources.items.walls_texture; // desktop texture
-        // }
-
+        if (window.mobileAndTabletCheck()) {
+            this.walls_texture = this.resources.items.mobile_walls_texture; // mobile texture
+        } else {
+            this.walls_texture = this.resources.items.walls_texture; // desktop texture
+        }
     }
 
     setMaterials() {
-        // this.walls_texture.flipY = false;
-        // this.walls_texture.encoding = THREE.sRGBEncoding;
+        this.walls_texture.flipY = false;
+        this.walls_texture.encoding = THREE.sRGBEncoding;
 
-        // this.walls.children.find((child) => {
-        //     child.material = new THREE.MeshBasicMaterial({
-        //         map: this.walls_texture,
-        //     });
-        // });
+        this.walls.children.find((child) => {
+            child.material = new THREE.MeshBasicMaterial({
+                map: this.walls_texture,
+            });
+        });
 
         this.scene.add(this.walls);
     }
